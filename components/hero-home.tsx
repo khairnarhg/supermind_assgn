11984f110416dd3409ca1d3d7cc060551612dac5
khairnarhg <hgkhairnar255@outlook.com>
2025-01-15 00:08:39 +0530

    HK rebuilding 4

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -15,7 +15,7 @@ export default function HeroHome() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-6xl"
               data-aos="fade-up"
             >
-              Socialytics ✨
+              Socialytix ✨
             </h1>
             <h1
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-4xl"
